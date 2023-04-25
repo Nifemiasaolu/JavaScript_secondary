df459e6e92d6f917f88d84d8cf9ebcdc31c73823
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-25 11:00:50 +0100

    Looping Array, Continue and Break

--- script.js
+++ script.js
@@ -337,11 +337,42 @@
     // console.log (jonas.length);
     for (let i = 0; i < jonas.length; i++) {
         // Reading from jonas array 
-        console.log (jonas[i], typeof jonas[i]);
+        // console.log (jonas[i], typeof jonas[i]);
         
         // Filling types array 
-        types[i] = typeof jonas[i];
-        types.push (typeof jonas[1]);
+        // types[i] = typeof jonas[i];
+        types.push (typeof jonas[i]);
     }
 
     console.log(types);    
+
+
+    /////////////////
+    // How to Loop Through Arrays
+    const years = [1991, 2007, 1969, 2020];
+    const ages = [];
+
+    for (let i = 0; i < years.length; i++) {
+        ages.push(2037 - years[i]);
+        // or 
+        // ages[i] = (2037  - years[i]);
+    }
+
+    console.log(ages);
+
+
+    /////////////////
+    // Continue and Break Statement 
+    console.log('---ONLY STRINGS---');
+    for (let i = 0; i < jonas.length; i++) {
+        if (typeof jonas[i] != 'string') continue;
+        console.log (jonas[i], typeof jonas[i]);
+    }
+    
+    
+    console.log('---BREAK WITH NUMBER---');
+    for (let i = 0; i < jonas.length; i++) {
+        if (typeof jonas[i] === 'number') break; 
+        console.log (jonas[i], typeof jonas[i]);
+    }
+    
